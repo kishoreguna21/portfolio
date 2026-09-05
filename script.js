c1025436feca0952fa3d94bb38fae9f52896a04f
Kishore G <kishoreguna21@gmail.com>
2026-09-05 19:14:49 +0530

    Update portfolio design and profile

--- script.js
+++ script.js
@@ -1,124 +1,49 @@
-/* =========================
-   PORTFOLIO JAVASCRIPT
-========================= */
+function toggleMenu() {
 
-console.log("Kishore G Portfolio loaded successfully!");
+    const nav = document.getElementById("navMenu");
 
+    nav.classList.toggle("active");
 
-
-/* =========================
-   MOBILE MENU
-========================= */
-
-const menuBtn = document.getElementById("menuBtn");
-
-const navLinks = document.getElementById("navLinks");
+}
 
 
-menuBtn.addEventListener("click", function () {
+document.querySelectorAll("#navMenu a").forEach(link => {
 
-    navLinks.classList.toggle("active");
+    link.addEventListener("click", () => {
 
-});
-
-
-
-/* =========================
-   CLOSE MENU AFTER CLICK
-========================= */
-
-const links = document.querySelectorAll(".nav-links a");
-
-
-links.forEach(function (link) {
-
-    link.addEventListener("click", function () {
-
-        navLinks.classList.remove("active");
+        document.getElementById("navMenu")
+            .classList.remove("active");
 
     });
 
 });
 
-
-
-/* =========================
-   BACK TO TOP
-========================= */
 
 const topBtn = document.getElementById("topBtn");
 
 
-window.addEventListener("scroll", function () {
+window.addEventListener("scroll", () => {
 
     if (window.scrollY > 400) {
 
-        topBtn.classList.add("show");
+        topBtn.style.display = "block";
 
     } else {
 
-        topBtn.classList.remove("show");
+        topBtn.style.display = "none";
 
     }
 
 });
 
 
-topBtn.addEventListener("click", function () {
+function scrollToTop() {
 
     window.scrollTo({
+
         top: 0,
         behavior: "smooth"
+
     });
 
-});
-
-
-
-/* =========================
-   SCROLL REVEAL
-========================= */
-
-const cards = document.querySelectorAll(
-    ".skill-card, .project-card, .certificate-card, .timeline-item, .about-card, .education-card"
-);
-
-
-const observer = new IntersectionObserver(
-
-    function (entries) {
-
-        entries.forEach(function (entry) {
-
-            if (entry.isIntersecting) {
-
-                entry.target.style.opacity = "1";
-
-                entry.target.style.transform =
-                    "translateY(0)";
-
-            }
-
-        });
-
-    },
-
-    {
-        threshold: 0.1
-    }
-
-);
-
-
-cards.forEach(function (card) {
-
-    card.style.opacity = "0";
-
-    card.style.transform = "translateY(25px)";
-
-    card.style.transition =
-        "opacity 0.7s ease, transform 0.7s ease";
-
-    observer.observe(card);
-
-});
+}
